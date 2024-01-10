b7f3ecac36b779d8d425a1930ae894985deb56a5
Adarsh Raut <adarshraut0987@gmail.com>
2024-01-11 00:10:11 +0530

    add user routes

---
+++ server/middleware/auth.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header('Authorization');
+
+    if (!token) {
+      return res.status(403).send('Access denied');
+    }
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
